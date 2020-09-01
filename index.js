5bbefa3a52d56f72685f476e4883e3f5b1d0859e
AnweshDahal <anweshd2000@gmail.com>
2020-09-01 16:59:55 +0545

    Initialized Project

---
+++ index.js
@@ -0,0 +1,39 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindows() {
+  // let splashScreen = new BrowserWindow(
+  //   {
+  //     width: 400,
+  //     height: 400,
+  //     resizable: false,
+  //     movable: false,
+  //     frame: false
+  //   }
+  // );
+
+  let mainWindow = new BrowserWindow({
+    width: 700,
+    height: 800,
+    resizable: false,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  mainWindow.loadFile("./main.html");
+
+  mainWindow.on('closed', () => {
+    app.quit();
+  })
+
+  // splashScreen.loadFile("./splashScreen.html");
+
+  // splashScreen.on('closed', () => {
+  //   app.quit()
+  // });
+}
+
+app.on('ready', () => {
+  createWindows();
+});
+
